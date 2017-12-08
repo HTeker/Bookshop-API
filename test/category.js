9695cdcc46812d9bc98cbe8bf60b154ef39d809b
Halil Teker <h.teker.developer>
2017-12-08 16:10:04 +0100

    Restructured category test

--- test/category.js
+++ test/category.js
@@ -6,30 +6,35 @@ let chai = require('chai'),
 	should = chai.should(),
 	server = require('../server'),
 	request = require('co-supertest')
-	db = require('../data/db');
+	db = require('../data/db')
+	Category = require('../models/category');
 
 describe('Category', () => {
 
 	beforeEach(function(done){
 		db.sync({force: true}).then(function(){
-			done();
+			Category.bulkCreate([
+				{ name: 'Fiction' },
+				{ name: 'Biography' },
+				{ name: 'Humour' },
+				{ name: 'Computing' },
+				{ name: 'Sport' }
+			]).then((categories) => {
+				done();
+			});
 		});
 	});
 
 	it('get all categories', function*(){
 		let categories = (yield request(server).get('/category').expect(200).end()).body;
-		categories.should.be.a('array');
+		categories.should.have.lengthOf(5);
 	});
 
 	it('create a category', function*(){
-		let categories = (yield request(server).get('/category').expect(200).end()).body;
-
 		let new_category = { name: 'Programming'};
 		yield request(server).post('/category').send(new_category).expect(201).end();
-
-		let newCategories = (yield request(server).get('/category').expect(200).end()).body;
-
-		newCategories.should.have.lengthOf(categories.length + 1);
+		let categories = (yield request(server).get('/category').expect(200).end()).body;
+		categories.should.have.lengthOf(6);
 	});
 
 });
