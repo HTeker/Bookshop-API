5204e32a13685d986d4870d72db6f10043f4f4e9
Halil <h.teker.developer@gmail.com>
2017-12-17 21:20:40 +0100

    Added user model validation

--- models/user.js
+++ models/user.js
@@ -9,15 +9,37 @@ const User = db.define('User', {
 	},
 	name: {
 		type: Sequelize.STRING,
-		allowNull: false
+		allowNull: false,
+		validate: {
+			len: {
+				args: [3, 100],
+				msg: "Name should have a length between 3 and 100 characters"
+			}
+		}
 	},
 	email: {
 		type: Sequelize.STRING,
-		allowNull: false
+		allowNull: false,
+		validate: {
+			isEmail: {
+				args: [3, 100],
+				msg: "E-mail should be a valid e-mailaddress"
+			}
+		}
 	},
 	password: {
 		type: Sequelize.STRING,
-		allowNull: false
+		allowNull: false,
+		validate: {
+			len: {
+				args: [8, 35],
+				msg: "Password should have a length between 8 and 35 characters"
+			},
+			is: {
+				args: "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)",
+				msg: "Password should contain at least 1 lower case character, 1 upper case character and 1 digit"
+			}
+		}
 	}
 });
 
