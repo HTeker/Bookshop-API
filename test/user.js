c376b21e61d4fc88151a46c6441c5ccf601ec589
Halil Teker <h.teker.developer>
2017-12-19 11:47:15 +0100

    Changed test to pass

--- test/user.js
+++ test/user.js
@@ -15,11 +15,11 @@ describe('User', () => {
 	beforeEach(function(done){
 		db.sync({force: true}).then(function(){
 			User.bulkCreate([
-				{name: 'Halil', email: 'halil@example.com', password: '12345678'},
-				{name: 'Teker', email: 'teker@example.com', password: '12345678'},
-				{name: 'Ibrahim', email: 'ibrahim@example.com', password: '12345678'},
-				{name: 'Jack', email: 'jack@example.com', password: '12345678'},
-				{name: 'Michael', email: 'michael@example.com', password: '12345678'}
+				{name: 'Halil', email: 'halil@example.com', password: '1234Pass5678'},
+				{name: 'Teker', email: 'teker@example.com', password: '1234Pass5678'},
+				{name: 'Ibrahim', email: 'ibrahim@example.com', password: '1234Pass5678'},
+				{name: 'Jack', email: 'jack@example.com', password: '1234Pass5678'},
+				{name: 'Michael', email: 'michael@example.com', password: '1234Pass5678'}
 			]).then((users) => {
 				done();
 			});
@@ -33,7 +33,7 @@ describe('User', () => {
 	});
 
 	it('create a user', function*(){
-		yield request(server).post('/user').send({ name: 'User #', email: 'email@example.com', password: '123456'}).expect(201).end();
+		yield request(server).post('/user').send({ name: 'User #', email: 'email@example.com', password: '1234Pass5678'}).expect(201).end();
 		let users = (yield request(server).get('/user').expect(200).end()).body;
 		users.should.have.lengthOf(6);
 	});
@@ -42,7 +42,7 @@ describe('User', () => {
 		let user = (yield request(server).get('/user/3').expect(200).end()).body;
 		delete user.createdAt;
         delete user.updatedAt;
-		user.should.deep.equal({id: 3, name: 'Ibrahim', email: 'ibrahim@example.com', password: '12345678'});
+		user.should.deep.equal({id: 3, name: 'Ibrahim', email: 'ibrahim@example.com', password: '1234Pass5678'});
 	});
 
 	it('delete a user by id', function*(){
@@ -55,7 +55,7 @@ describe('User', () => {
 		const user = (yield request(server).put('/user/4').send({ name: 'Updated User' }).expect(200).end()).body;
 		delete user.createdAt;
         delete user.updatedAt;
-		user.should.deep.equal({ id: 4, name: 'Updated User', email: 'jack@example.com', password: '12345678' });
+		user.should.deep.equal({ id: 4, name: 'Updated User', email: 'jack@example.com', password: '1234Pass5678' });
 	});
 
 	it('search users by name and email', function*(){
