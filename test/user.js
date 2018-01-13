fcb694f0991637169d29b274920c64246f37d8c6
Halil <h.teker.developer@gmail.com>
2018-01-13 17:28:30 +0100

    Added login in requests

--- test/user.js
+++ test/user.js
@@ -10,12 +10,14 @@ let chai = require('chai'),
 	seeder = require('../data/seeder')
 	User = require('../models/user');
 
+var token;
+
 describe('User', () => {
 
 	beforeEach(function(done){
 		db.sync({force: true}).then(function(){
 			User.bulkCreate([
-				{name: 'Halil', email: 'halil@example.com', password: '1234Pass5678'},
+				{name: 'Halil', email: 'halil@example.com', isAdmin: true, password: '1234Pass5678'},
 				{name: 'Teker', email: 'teker@example.com', password: '1234Pass5678'},
 				{name: 'Ibrahim', email: 'ibrahim@example.com', password: '1234Pass5678'},
 				{name: 'Jack', email: 'jack@example.com', password: '1234Pass5678'},
@@ -26,20 +28,23 @@ describe('User', () => {
 		});
 	});
 
+	it('get user token', function*(){
+		token = (yield request(server).post('/login').send({email: 'halil@example.com', password: '1234Pass5678'}).expect(200).end()).body.token;
+	});
+
 	it('get all users', function*(){
-		let users = (yield request(server).get('/user').expect(200).end()).body;
+		let users = (yield request(server).get('/user').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.be.a('array');
 		users.should.have.lengthOf(5);
 	});
 
 	it('create a user', function*(){
-		yield request(server).post('/user').send({ name: 'User #', email: 'email@example.com', password: '1234Pass5678'}).expect(201).end();
-		let users = (yield request(server).get('/user').expect(200).end()).body;
+		yield request(server).post('/user').send({ name: 'User #', email: 'email@example.com', password: '1234Pass5678'}).set('Authorization', 'Bearer ' + token).expect(201).end();
+		let users = (yield request(server).get('/user').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.have.lengthOf(6);
 	});
 
 	it('get a user by email', function*(){
-		const token = (yield request(server).post('/login').send({email: 'ibrahim@example.com', password: '1234Pass5678'}).expect(200).end()).body;
 		let user = (yield request(server).get('/user/ibrahim@example.com').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		delete user.createdAt;
         delete user.updatedAt;
@@ -48,14 +53,12 @@ describe('User', () => {
 	});
 
 	it('delete a user by email', function*(){
-		const token = (yield request(server).post('/login').send({email: 'halil@example.com', password: '1234Pass5678'}).expect(200).end()).body;
 		yield request(server).delete('/user/halil@example.com').set('Authorization', 'Bearer ' + token).expect(200).end();
-		let users = (yield request(server).get('/user').expect(200).end()).body;
+		let users = (yield request(server).get('/user').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.have.lengthOf(4);
 	});
 
 	it('update a user by email', function*(){
-		const token = (yield request(server).post('/login').send({email: 'jack@example.com', password: '1234Pass5678'}).expect(200).end()).body;
 		const user = (yield request(server).put('/user/jack@example.com').send({ name: 'Updated User' }).set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		delete user.createdAt;
         delete user.updatedAt;
@@ -64,13 +67,13 @@ describe('User', () => {
 	});
 
 	it('search users by name and email', function*(){
-		let users = (yield request(server).get('/user/search/example').expect(200).end()).body;
+		let users = (yield request(server).get('/user/search/example').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.have.lengthOf(5);
 
-		users = (yield request(server).get('/user/search/i').expect(200).end()).body;
+		users = (yield request(server).get('/user/search/i').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.have.lengthOf(3);
 
-		users = (yield request(server).get('/user/search/halil').expect(200).end()).body;
+		users = (yield request(server).get('/user/search/halil').set('Authorization', 'Bearer ' + token).expect(200).end()).body;
 		users.should.have.lengthOf(1);
 	});
 
