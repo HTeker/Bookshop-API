09e645372eb7f152f89498ba8a36ee25fbdc0967
Halil Teker <HTeker@users.noreply.github.com>
2018-11-29 16:02:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ var express = require('express'),
 
 Associations.associate();
 
-if(process.env.NODE_ENV != 'test'){
+/*if(process.env.NODE_ENV != 'test'){
     db.sync({force: true}).then(function(){
         seeder.seed();
     });
-}
+}*/
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
