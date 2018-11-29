c9df1b8870c25efcf5d0420297febf0f5c70b321
Halil Teker <HTeker@users.noreply.github.com>
2018-11-29 16:02:11 +0100

    Update db.js

--- data/db.js
+++ data/db.js
@@ -1,31 +1,38 @@
 const Sequelize = require('sequelize');
 
-var logging = true;
+let db;
 
-if(process.env.NODE_ENV == 'test'){
-	logging = false;
+switch(process.env.NODE_ENV.trim()) {
+
+	case: 'test':
+		
+		console.log('Connecting to test database...');
+
+		db = new Sequelize(process.env.DATABASE_URL, {
+		    dialect: 'postgres',
+		    dialectOptions: {
+			ssl: true
+		    }
+		});
+
+		break;
+		
+	default:
+		
+		console.log('Connecting to default database...');
+
+		db = new Sequelize({
+		    host: process.env.DATABASE_URL || 'localhost',
+		    port: 5432,
+		    database: 'webapp',
+		    username: 'root',
+		    password: 'root',
+		    dialect: 'postgres',
+		    logging: false
+		});
+
+		break;
+		
 }
-
-/*const db = new Sequelize({
-    host: 'sql11.freemysqlhosting.net',
-    port: 3306,
-    database: 'sql11207449',
-    username: 'sql11207449',
-    password: 'HGUbVe47Vx',
-    dialect: 'mysql',
-    logging: logging
-});*/
-
-const db = new Sequelize({
-    host: process.env.DATABASE_URL || 'localhost',
-    port: 5432,
-    database: 'webapp',
-    username: 'root',
-    password: 'root',
-    dialect: 'postgres',
-    logging: logging
-});
-
-//db.sync({force: true});
 
 module.exports = db;
